987e6d10ba18f5841290090a1fbe47a978bc1b9b
unknown <va.klymenko1996@gmail.com>
2020-12-02 11:09:59 -0800

    Added net test case  CalculatorTest2.xml

--- src/test/java/core/CalculatorTest2.java
+++ src/test/java/core/CalculatorTest2.java
@@ -9,19 +9,19 @@ import org.junit.runners.MethodSorters;
 public class CalculatorTest2 {
 
     @Test(timeout=1000) // @Ignore
-    public void test_01_div_4() {
-        System.out.println("Test # 01: Divide 4 params");
+    public void test_06_div_4() {
+        System.out.println("Test # 06: Divide 4 params");
         assertEquals("Not correct", Calculator.divide(200.0, 2.0, 25.0, 2.0), 2.0, 0.09);}
 
     @Test(timeout=1000) // @Ignore
-    public void test_02_add_3() {
-        System.out.println("Test # 02: Add 3 params");
-        assertEquals("Not correct", Calculator.add(8.0, 5.0, 7.0), 20.0, 0.09);}
+    public void test_05_div_3() {
+        System.out.println("Test # 05: Divide 3 params");
+        assertEquals("Not correct", Calculator.divide(8.0, 4.0, 2.0), 1.0, 0.09);}
 
     @Test(timeout=1000) // @Ignore
-    public void test_03_add_4() {
-        System.out.println("Test # 03: Add 4 params");
-        assertEquals("Not correct", Calculator.add(185.3, 0.7, 10.0, 74.0), 270.0, 0.09);}
+    public void test_07_miltiply_2() {
+        System.out.println("Test # 07: Multiply 4 params");
+        assertEquals("Not correct", Calculator.multiply(44.00, 25.00), 1100.0, 0.09);}
 
     @Test(timeout=1000) // @Ignore
     public void test_04_divide_2() {
@@ -29,42 +29,30 @@ public class CalculatorTest2 {
         assertEquals("Not correct", Calculator.divide(90.0, 30.0), 3.0, 0.09);}
 
     @Test(timeout=1000) // @Ignore
-    public void test_05_divide_3() {
-        System.out.println("Test # 05: Divide 3 params");
-        assertEquals("Not correct", Calculator.divide(40.0, 8.0, 2.0), 2.5, 0.09);}
+    public void test_01_add_2() {
+        System.out.println("Test # 01: Add 2 params");
+        assertEquals("Not correct", Calculator.add(40.0, 8.0), 48.0, 0.09);}
 
     @Test(timeout=1000) // @Ignore
-    public void test_06_divide_4() {
-        System.out.println("Test # 06: Divide 4 params");
-        assertEquals("Not correct", Calculator.divide(600.0, 50.0, 4.0, 2.0), 1.5, 0.09);}
+    public void test_09_multiply_4() {
+        System.out.println("Test # 09: Mu;tiply 4 params");
+        assertEquals("Not correct", Calculator.multiply(600.0, 50.0, 4.0, 2.0), 240000.0, 0.09);}
 
     @Test(timeout=1000) // @Ignore
-    public void test_07_multiply_2() {
-        System.out.println("Test # 07: Multiply 2 params");
-        assertEquals("Not correct", Calculator.multiply(9.0, 5.0), 45.0, 0.09);}
+    public void test_02_add_3() {
+        System.out.println("Test # 02: Add 3 params");
+        assertEquals("Not correct", Calculator.add(9.0, 5.0,3.0), 17.0, 0.09);}
+
+    @Test(timeout=1000) // @Ignore
+    public void test_04_add_4() {
+        System.out.println("Test # 04: Add 5 params");
+        assertEquals("Not correct", Calculator.add(20.0, 5.0, 8.0,7.0), 40.0, 0.09);}
 
     @Test(timeout=1000) // @Ignore
     public void test_08_multiply_3() {
         System.out.println("Test # 08: Multiply 3 params");
-        assertEquals("Not correct", Calculator.multiply(20.0, 5.0, 8.0), 800.0, 0.09);}
+        assertEquals("Not correct", Calculator.multiply(24.0, 2.0, 10.0), 480.0, 0.09);}
 
-    @Test(timeout=1000) // @Ignore
-    public void test_09_multiply_4() {
-        System.out.println("Test # 09: Multiply 4 params");
-        assertEquals("Not correct", Calculator.multiply(24.0, 4.0, 10.0, 5.0), 4800.0, 0.09);}
 
-    @Test(timeout=1000) // @Ignore
-    public void test_10_subtract_2() {
-        System.out.println("Test # 10: Subtract 2 params");
-        assertEquals("Not correct", Calculator.subtract(28.0, 14.0), 14.0, 0.09);}
 
-    @Test(timeout=1000) // @Ignore
-    public void test_11_subtract_3() {
-        System.out.println("Test # 11: Subtract 3 params");
-        assertEquals("Not correct", Calculator.subtract(220.0, 20.0, 120.0), 80.0, 0.09);}
-
-    @Test(timeout=1000) // @Ignore
-    public void test_12_subtract_4() {
-        System.out.println("Test # 12: Subtract 4 params");
-        assertEquals("Not correct", Calculator.subtract(400.0, 150.0, 50.0, 45.0), 155.0, 0.09);}
 }
